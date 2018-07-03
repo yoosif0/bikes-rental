244e78dae187abfff1fecdca9424d426cc078602
Youssef Sherif <sharief@aucegypt.edu>
2018-07-03 16:39:29 +0200

    Update rxjs refs

--- Client/src/epics/index.js
+++ Client/src/epics/index.js
@@ -4,12 +4,10 @@ import 'rxjs/add/operator/mapTo';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/mergeMap';
-// import 'rxjs/add/operator/concat';
 import 'rxjs/add/operator/takeUntil';
-import 'rxjs/add/operator/throttleTime';
 import 'rxjs/add/operator/auditTime';
-import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/concat';
 
