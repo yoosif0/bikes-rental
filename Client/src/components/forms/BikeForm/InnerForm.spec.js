b53f8e92916f34b68727a5c9898e0268d23e9d14
Youssef Sherif <sharief@aucegypt.edu>
2018-07-14 17:44:49 +0200

    Fix tests

--- Client/src/components/forms/BikeForm/InnerForm.spec.js
+++ Client/src/components/forms/BikeForm/InnerForm.spec.js
@@ -1,20 +1,19 @@
 import { shallow } from 'enzyme';
 import React from 'react'
 import DefaultInput from '../../ui-inputs/DefaultInputLayout';
-import { SubmitButton } from '../../buttons/SubmitButton';
 import { InnerForm } from './InnerForm';
 import { Field } from 'formik';
 
 describe('<InnerForm />', () => {
-    it('should render 5 <DefaultInput /> and 5 Field components', () => {
-        const wrapper = shallow(<InnerForm errors={{}} touched={{}} isSubmitting={false} dirty={true} />);
-        expect(wrapper.find(DefaultInput).length).toBe(5);
-        expect(wrapper.find(Field).length).toBe(5);
-    });
-
-    it('should render 1 <SubmitButton /> components', () => {
-        const wrapper = shallow(<InnerForm errors={{}} touched={{}} isSubmitting={false} dirty={true} />);
-        expect(wrapper.find(SubmitButton).length).toBe(1);
+    it('should render 4 <DefaultInput /> and 4 Field components', () => {
+        const wrapper = shallow(
+        <InnerForm errors={{}} touched={{}} isSubmitting={false} dirty={true} values ={{}}> 
+            {
+                () => <li> AAAAA </li>
+            }
+        </InnerForm>);
+        expect(wrapper.find(DefaultInput).length).toBe(4);
+        expect(wrapper.find(Field).length).toBe(4);
     });
 
 
