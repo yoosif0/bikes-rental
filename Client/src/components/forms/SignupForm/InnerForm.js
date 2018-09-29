1302c31c0b742b08f7ef14cab20801afd6d3dd88
Youssef Sherif <sharief@aucegypt.edu>
2018-09-29 06:42:28 +0200

    Use prettier

--- Client/src/components/forms/SignupForm/InnerForm.js
+++ Client/src/components/forms/SignupForm/InnerForm.js
@@ -1,43 +1,62 @@
-import React from 'react'
-import DefaultInput from '../../ui-inputs/DefaultInputLayout';
-import SubmitButton from '../../buttons/SubmitButton';
-import { Form, Field } from 'formik';
-import { InputFeedback } from '../../text/InputFeedback';
-import { captchaSiteKey } from '../../../config/constants';
+import React from "react";
+import DefaultInput from "../../ui-inputs/DefaultInputLayout";
+import SubmitButton from "../../buttons/SubmitButton";
+import { Form, Field } from "formik";
+import { InputFeedback } from "../../text/InputFeedback";
+import { captchaSiteKey } from "../../../config/constants";
 import ReCAPTCHA from "react-google-recaptcha";
-
 
 let recaptchaInstance;
 
-export const InnerForm = ({ errors, touched, isSubmitting, dirty, setFieldValue }) => {
-    if(errors.recaptchaExpired){
-        recaptchaInstance.reset();
-        errors.recaptchaExpired = false
-    }
-    return (
-        <Form>
-            <DefaultInput label="Name">
-                <Field type="text" name="name" className="form-control" />
-                <InputFeedback error={touched.name && errors.name} />
-            </DefaultInput>
-            <DefaultInput label="Email">
-                <Field type="text" name="email" className="form-control" />
-                <InputFeedback error={touched.email && errors.email} />
-            </DefaultInput>
-            <DefaultInput label="Password">
-                <Field type="password" name="password" className="form-control" />
-                <InputFeedback error={touched.password && errors.password} />
-            </DefaultInput>
-            <DefaultInput label="Confirm Password">
-                <Field type="password" name="passwordConfirm" className="form-control" />
-                <InputFeedback error={touched.passwordConfirm && errors.passwordConfirm} />
-            </DefaultInput>
-            <ReCAPTCHA name="recaptcha"
-            className="mb-4" 
-                ref={el => { recaptchaInstance = el; }}
-                sitekey={captchaSiteKey}
-                onChange={(response) => setFieldValue("recaptcha", response)} />,
-        <SubmitButton classNames="mt-4" disabled={!dirty || isSubmitting || Object.keys(errors).length} ></SubmitButton>
-        </Form>
-    )
-}
+export const InnerForm = ({
+  errors,
+  touched,
+  isSubmitting,
+  dirty,
+  setFieldValue
+}) => {
+  if (errors.recaptchaExpired) {
+    recaptchaInstance.reset();
+    errors.recaptchaExpired = false;
+  }
+  return (
+    <Form>
+      <DefaultInput label="Name">
+        <Field type="text" name="name" className="form-control" />
+        <InputFeedback error={touched.name && errors.name} />
+      </DefaultInput>
+      <DefaultInput label="Email">
+        <Field type="text" name="email" className="form-control" />
+        <InputFeedback error={touched.email && errors.email} />
+      </DefaultInput>
+      <DefaultInput label="Password">
+        <Field type="password" name="password" className="form-control" />
+        <InputFeedback error={touched.password && errors.password} />
+      </DefaultInput>
+      <DefaultInput label="Confirm Password">
+        <Field
+          type="password"
+          name="passwordConfirm"
+          className="form-control"
+        />
+        <InputFeedback
+          error={touched.passwordConfirm && errors.passwordConfirm}
+        />
+      </DefaultInput>
+      <ReCAPTCHA
+        name="recaptcha"
+        className="mb-4"
+        ref={el => {
+          recaptchaInstance = el;
+        }}
+        sitekey={captchaSiteKey}
+        onChange={response => setFieldValue("recaptcha", response)}
+      />
+      ,
+      <SubmitButton
+        classNames="mt-4"
+        disabled={!dirty || isSubmitting || Object.keys(errors).length}
+      />
+    </Form>
+  );
+};
--- Client/src/components/layout/Navbar.js
+++ Client/src/components/layout/Navbar.js
@@ -1,75 +1,94 @@
-import React from 'react'
-import { NavLink, Link, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { unPersistMyInfo } from '../../services/localStorage';
-import { compose } from 'redux';
+import React from "react";
+import { NavLink, Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { unPersistMyInfo } from "../../services/localStorage";
+import { compose } from "redux";
 
 const NavBarLink = ({ to, label }) => (
-    <li className="nav-item" style={{marginRight:'20px'}}>
-        <NavLink activeClassName="active" to={to} className="nav-link">{label}</NavLink>
-    </li>
-)
-
+  <li className="nav-item" style={{ marginRight: "20px" }}>
+    <NavLink activeClassName="active" to={to} className="nav-link">
+      {label}
+    </NavLink>
+  </li>
+);
 
 export const PNavbar = ({ isManager, isAuthenticated, logout, myId }) => (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
-        <NavLink className="navbar-brand" to="/"> Bikes Rental © </NavLink>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto" >
-                {
-                    isAuthenticated ?
-                        <React.Fragment>
-                            <NavBarLink to='/myProfile' label="My profile" />
-                            <NavBarLink to='/bikes/listing' label="Bike Listing" />
-                            <NavBarLink to='/map' label="Bikes Map" />
-                            <NavBarLink to={`/upcomingReservations?userId=${myId}&label=My`} label="My Upcoming Reservations" />
-                            <NavBarLink to={`/pastReservations?userId=${myId}&label=My`} label="My Past Reservations" />
-                            {/* <NavBarLink to='/myPreviouslyUsedBikes' label="My Previously Used Bikes" /> */}
-                        </React.Fragment>
-                        :
-                        <React.Fragment>
-                            <NavBarLink to='/map' label="Login" />
-                            <NavBarLink to='/signup' label="Signup" />
-                        </React.Fragment>
-                }
-                {isManager &&
-                    <React.Fragment>
-                        <NavBarLink to='/addBike' label="Add new Bike" />
-                        <NavBarLink to='/users' label="Users" />
-                    </React.Fragment>
-                }
-            </ul>
-            {
-                isAuthenticated &&
-                <ul className="navbar-nav ml-auto mr-5" >
-                    <li className="nav-item" onClick={() => logout()} id="logout">
-                        <Link to="login" className="nav-link">Logout</Link>
-                    </li>
-                </ul>
-            }
-        </div>
-
-    </nav>
-)
+  <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
+    <NavLink className="navbar-brand" to="/">
+      {" "}
+      Bikes Rental ©{" "}
+    </NavLink>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+        {isAuthenticated ? (
+          <React.Fragment>
+            <NavBarLink to="/myProfile" label="My profile" />
+            <NavBarLink to="/bikes/listing" label="Bike Listing" />
+            <NavBarLink to="/map" label="Bikes Map" />
+            <NavBarLink
+              to={`/upcomingReservations?userId=${myId}&label=My`}
+              label="My Upcoming Reservations"
+            />
+            <NavBarLink
+              to={`/pastReservations?userId=${myId}&label=My`}
+              label="My Past Reservations"
+            />
+            {/* <NavBarLink to='/myPreviouslyUsedBikes' label="My Previously Used Bikes" /> */}
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <NavBarLink to="/map" label="Login" />
+            <NavBarLink to="/signup" label="Signup" />
+          </React.Fragment>
+        )}
+        {isManager && (
+          <React.Fragment>
+            <NavBarLink to="/addBike" label="Add new Bike" />
+            <NavBarLink to="/users" label="Users" />
+          </React.Fragment>
+        )}
+      </ul>
+      {isAuthenticated && (
+        <ul className="navbar-nav ml-auto mr-5">
+          <li className="nav-item" onClick={() => logout()} id="logout">
+            <Link to="login" className="nav-link">
+              Logout
+            </Link>
+          </li>
+        </ul>
+      )}
+    </div>
+  </nav>
+);
 
 const mapStateToProps = state => ({
-    myId: state.authStoreState.id,
-    isAuthenticated: state.authStoreState.isAuthenticated,
-    isManager: state.authStoreState.role === 'manager'
-})
+  myId: state.authStoreState.id,
+  isAuthenticated: state.authStoreState.isAuthenticated,
+  isManager: state.authStoreState.role === "manager"
+});
 
 const mapDispatchToProps = dispatch => ({
-    logout: () => {
-        unPersistMyInfo()
-        dispatch({ type: 'LOGGED_OUT' })
-    }
-})
+  logout: () => {
+    unPersistMyInfo();
+    dispatch({ type: "LOGGED_OUT" });
+  }
+});
 
-export const Navbar = compose(withRouter, connect(mapStateToProps, mapDispatchToProps))(PNavbar)
-
-
-
+export const Navbar = compose(
+  withRouter,
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
+)(PNavbar);
